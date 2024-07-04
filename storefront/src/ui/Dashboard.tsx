54252306d6bca5badef558a32e78e3b3bef12c63
Andrei Soroker <andrei@fogbender.com>
2024-07-04 12:36:00 -0700

    Use React for theme controller

--- storefront/src/ui/Dashboard.tsx
+++ storefront/src/ui/Dashboard.tsx
@@ -7,6 +7,8 @@ import { QueryClientProvider, useMutation, useQuery } from "@tanstack/react-quer
 import { Title } from "reactjs-meta";
 import wretch from "wretch";
 import { apiServer, queryClient, queryKeys } from "./client";
+import SupafanaLogo from "./landing/assets/logo.svg?url";
+import ThemeController from "./ReactThemeController";
 
 import { getServerUrl } from "../config";
 
@@ -49,33 +51,49 @@ export const Dashboard = () => {
   console.log(organizationId, organizations);
 
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      {organizations ? (
-        <div>
-          {organizations.map(o => (
-            <div key={o.id}>
-              <div>{o.name} (organization)</div>
-              {projects?.map(p => (
-                <div key={p.id}>
-                  <div>{p.name} (project) - {p.id} - {p.status}</div>
-                </div>
-              ))}
-            </div>
-          ))}
+    <div className="h-full flex flex-col">
+      <div className="bg-transparent sticky top-0 z-20">
+        <div
+          className="nav-container-blur bg-white absolute z-[-1] h-full w-full shadow-[0_2px_4px_rgba(0,0,0,.02),0_1px_0_rgba(0,0,0,.06)] dark:shadow-[0_-1px_0_rgba(255,255,255,.1)_inset] dark:bg-black dark:contrast-more:shadow-[0_0_0_1px_#000] contrast-more:shadow-[0_0_0_1px_#fff]"
+        >
+        </div>
+        <nav
+          className="mx-auto flex h-16 max-w-[90rem] items-center justify-end gap-2 pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]"
+        >
+          <div className="flex items-center mr-auto gap-x-5">
+            <img src={SupafanaLogo} alt="supafana-logo" width={35} height={35} />
+          </div>
+          <ThemeController />
+        </nav>
+      </div>
+      <div className="flex-1 flex items-center justify-center">
+        {organizations ? (
+          <div>
+            {organizations.map(o => (
+              <div key={o.id}>
+                <div>{o.name} (organization)</div>
+                {projects?.map(p => (
+                  <div key={p.id}>
+                    <div>{p.name} (project) - {p.id} - {p.status}</div>
+                  </div>
+                ))}
+              </div>
+            ))}
 
-          <form method="post" action={signOutActionUrl}>
-            <button className="btn" type="submit">
-              Sign out
+            <form method="post" action={signOutActionUrl}>
+              <button className="btn" type="submit">
+                Sign out
+              </button>
+            </form>
+          </div>
+        ) : (
+          <form method="post" action={connectActionUrl}>
+            <button type="submit">
+              <img src="/connect-supabase-dark.svg" alt="Connect Supabase button" />
             </button>
           </form>
-        </div>
-      ) : (
-        <form method="post" action={connectActionUrl}>
-          <button type="submit">
-            <img src="/connect-supabase-dark.svg" alt="Connect Supabase button" />
-          </button>
-        </form>
-      )}
+        )}
+      </div>
     </div>
   );
 };
