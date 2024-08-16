a1c00b63c0f20152c31912dbe848165e2154eb95
Mikl Kurkov <mkurkov@gmail.com>
2024-08-16 23:39:20 +0300

    Update absdev1 server configuration

--- nix/hosts/supafana-absdev1.nix
+++ nix/hosts/supafana-absdev1.nix
@@ -3,7 +3,7 @@
 let
   env = "absdev1";
   domain = "${env}.supafana-test.com";
-  azureWebDomain = "todo.supafana-${env}.local";
+  azureWebDomain = "icy-ground-03d19110f.5.azurestaticapps.net";
 in
 {
   imports = [
@@ -18,11 +18,13 @@ in
   supafana.environment = {
     PG_HOST = "supafana-${env}-db.private.postgres.database.azure.com";
     PG_USER = "supafana-${env}-api";
-    PG_DATABASE = "supafana_${env}";
+    PG_DB = "supafana_${env}";
     PG_PASS = "AZURE_IDENTITY";
+    PG_SSL_ENABLE = "true";
     SUPAFANA_API_URL = "${domain}/api";
     SUPAFANA_STOREFRONT_URL = domain;
     SUPAFANA_AZURE_RESOURCE_GROUP = "supafana-${env}-rg";
+    SUPAFANA_TRIAL_LENGTH_MIN = "30";
   };
 
   sops.secrets."supafana.env" = {
